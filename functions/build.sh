9b34bdb9ec42eb56c2ac3c1d45118a3e0a5fa809
ContinentalBreakfast17 <hunterrhollant@gmail.com>
2022-11-23 13:42:18 -0500

    single rust package

--- functions/build.sh
+++ functions/build.sh
@@ -1,16 +1,4 @@
 #!/bin/bash -e
 
 echo "in build"
-
-makefiles=()
-while IFS= read -r line; do
-    makefiles+=( "$line" )
-done < <( find . -name "Makefile" -not -path "./_*-pkg/*" )
-
-for makefile in "${makefiles[@]}"
-do
-    dir=$(dirname "$makefile")
-    echo $dir
-    cd $dir && make
-    cd ..
-done
+make -C rust
